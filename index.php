24af52f135a4c80ef2b024ba206508289b37bc56
Winter003 <pkoss12@wp.pl>
2023-04-12 08:49:22 +0200

    start

---
+++ index.php
@@ -0,0 +1,153 @@
+<?php include('site/header.php'); ?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projekt01</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <img src="img/water.jpg" alt="img001">
+        <img src="img/water2.jpg" alt="img002">
+        <img src="img/water3.jpg" alt="img003">
+        <h1>Lorem, ipsum dolor.</h1>
+        <h1>Lorem, ipsum dolor sit.</h1>
+        <h1>Lorem, ipsum dolor sit amet.</h1>
+    </header>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    </div>
+<br>
+<br>
+<br>
+    <div class="container">
+        <div class="row">
+            <div class="col text-center box-margin">
+               
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md box-margin">
+            
+            <div class="card">
+  <img src="https://picsum.photos/300/200?random=3" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<br>
+<br>
+<br>
+            </div>
+            <div class="col-md box-margin">
+            
+            <div class="card">
+  <img src="https://picsum.photos/300/200?random=1" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+            </div>
+            <div class="col-md box-margin">
+            
+            <div class="card">
+  <img src="https://picsum.photos/300/200?random=2" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+            </div>
+        </div>
+     </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+            <div class="card">
+  <div class="card-body">
+    <h5 class="card-title">Zarejestruj się</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Jeśli nie masz konta zajerestruj się. Utworzenie konta daje dużo więcej możliwości.</p>
+    <a href="signup.php" class="card-link">Zarejestruj się</a>
+  </div>
+</div>
+
+            <div class="col">
+            <div class="card">
+  <div class="card-body">
+    <h5 class="card-title">Zaloguj</h5>
+    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="card-link">Card link</a>
+  </div>
+</div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+
+
+     <div class="p-5 mb-4 bg-light rounded-3">
+      <div class="container-fluid py-5">
+        <p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
+        <button class="btn btn-primary btn-lg" type="button">Example button</button>
+      </div>
+    </div>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+        <ul class="list-group">
+  <li class="list-group-item">An item</li>
+  <li class="list-group-item">A second item</li>
+  <li class="list-group-item">A third item</li>
+  <li class="list-group-item">A fourth item</li>
+  <li class="list-group-item">And a fifth one</li>
+</ul>
+</div>
+
+
+<div class="col">
+<ul class="list-group">
+  <li class="list-group-item">An item</li>
+  <li class="list-group-item">A second item</li>
+  <li class="list-group-item">A third item</li>
+  <li class="list-group-item">A fourth item</li>
+  <li class="list-group-item">And a fifth one</li>
+</ul>
+</div>
+
+
+<div class="col">
+<ul class="list-group">
+  <li class="list-group-item">An item</li>
+  <li class="list-group-item">A second item</li>
+  <li class="list-group-item">A third item</li>
+  <li class="list-group-item">A fourth item</li>
+  <li class="list-group-item">And a fifth one</li>
+</ul>
+</div>
+
+
+    <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+
+
+</html>
---
+++ signup.php
@@ -0,0 +1,124 @@
+<?php include('site/header.php'); ?>
+<?php
+$error = array("", "", "", "", "", true);
+if (isset($_POST['submit'])) {
+    $imie = htmlspecialchars($_POST['imie']);
+    if ($imie == "" || strlen($imie) < 3) {
+        $error[0] = "Brak imienia.";
+        // echo $error[0];
+    }
+    $nazwisko = htmlspecialchars($_POST['nazwisko']);
+    if ($nazwisko == "" || strlen($nazwisko) < 3) {
+        $error[1] = "Brak nazwiska.";
+    }
+    $mail = htmlspecialchars($_POST['mail']);
+    if ($mail == "") {
+        $error[2] = "Brak mail'a.";
+    }
+    $login = htmlspecialchars($_POST['login']);
+    if ($login == "" || strlen($login) < 2) {
+        $error[3] = "Brak loginu.";
+    }
+    $haslo1 = htmlspecialchars($_POST['haslo1']);
+    $haslo2 = htmlspecialchars($_POST['haslo2']);
+    if (
+        !preg_match('/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{8,20}$/', $haslo1)
+        || !preg_match('/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{8,20}$/', $haslo2)
+        || $haslo1 != $haslo2
+    )
+        $error[4] = "Hasło musi posiadać min. 8 znaków i być identyczne.";
+    // 1wwwdAw@dwaA!aa
+    
+    if (!empty($_POST['regulamin'])) {
+        $error[5] = true;
+    } else {
+        $error[5] = false;
+    }
+}
+?>
+
+<!-- formularz -->
+<link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <!-- <form action="signup.php" method="post"> -->
+            <form action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
+                <h2>Formularz</h2>
+                <div class="mb-3">
+                    <?php
+                    if ($error[0] != "") {
+                    ?>
+                        <label for="" class="alert alert-warning"><?php echo $error[0]; ?></label>
+                    <?php } ?>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Imię" name="imie">
+                </div>
+                <div class="mb-3">
+                    <?php
+                    if ($error[1] != "") {
+                    ?>
+                        <label for="" class="alert alert-warning"><?php echo $error[1]; ?></label>
+                    <?php } ?>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Nazwisko" name="nazwisko">
+                </div>
+                <div class="mb-3">
+                    <?php
+                    if ($error[3] != "") {
+                    ?>
+                        <label for="" class="alert alert-warning"><?php echo $error[3]; ?></label>
+                    <?php } ?>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Login" name="login">
+                </div>
+                <div class="mb-3">
+                    <?php
+                    if ($error[2] != "") {
+                    ?>
+                        <label for="" class="alert alert-warning"><?php echo $error[2]; ?></label>
+                    <?php } ?>
+                    <input type="mail" class="form-control" id="exampleFormControlInput1" 
+placeholder="nazwa@mail.com
+" name="mail">
+                </div>
+                <div class="mb-3">
+                    <?php
+                    if ($error[4] != "") {
+                    ?>
+                        <label for="" class="alert alert-warning"><?php echo $error[4]; ?></label>
+                    <?php } ?>
+                    <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="hasło" name="haslo1">
+                </div>
+                <div class="mb-3">
+                    <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="powtórz hasło" name="haslo2">
+                </div>
+                <div class="form-check">
+                      <?php 
+                      if (!$error[5]) {
+                      ?>
+                      <label for="" class="alert alert-warning">Musisz zaakceptować regulamin, aby korzystać z serwisu internetowego.</label>
+                      <?php } ?>
+                    <input class="form-check-input" type="checkbox" value="regulamin" id="flexCheckChecked" name="regulamin">
+                    <label class="form-check-label" for="flexCheckChecked">
+                        Akceptuje regulamin strony Internetowej.
+                    </label>
+                </div>
+                <div class="mb-3">
+                    <button type="submit" class="btn btn-primary mb-3" name="submit">Gotowe</button>
+                    <button type="reset" class="btn btn-primary mb-3">Wyczyść</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<!-- end - formularz -->
+<?php
+
+if($error[0] == "" && $error[1] == "" && $error[2] == "" && $error[3] == "" && $error[4] == "" && $error[5]) {
+  $conn = mysql_connect('localhost','webPLA','123asd','portal');
+  if ($conn) {
+    echo "Połączono z bazą danych";
+  }
+}
+
+?> 
