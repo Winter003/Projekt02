d4501b80053beefdca840efce2560dac10bda032
Winter003 <pkoss12@wp.pl>
2023-04-12 09:52:54 +0200

    start

--- index.php
+++ index.php
@@ -93,8 +93,8 @@
   <div class="card-body">
     <h5 class="card-title">Zaloguj</h5>
     <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="card-link">Card link</a>
+    <p class="card-text">Zaloguj się.</p>
+    <a href="loginup.php" class="card-link">Zaloguj się</a>
   </div>
 </div>
 </div>
---
+++ loginup.php
@@ -0,0 +1,36 @@
+<?php
+include('site/header.php');
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logowanie</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <form>
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Adres E-mail</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text"></div>
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Hasło</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <button type="submit" class="btn btn-primary">Zaloguj</button>
+    </form>
+    
+</body>
+</html>
