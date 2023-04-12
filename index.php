39394787e1e6374df63cdd8c373358b1414a825e
Winter003 <pkoss12@wp.pl>
2023-04-12 15:06:12 +0200

    poprawa

--- index.php
+++ index.php
@@ -107,7 +107,7 @@
      <div class="p-5 mb-4 bg-light rounded-3">
       <div class="container-fluid py-5">
         <p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
-        <button class="btn btn-primary btn-lg" type="button">Example button</button>
+        <a class="btn btn-primary btn-lg" type="button" href="kontakt.php">Napisz do nas!</a>
       </div>
     </div>
 
